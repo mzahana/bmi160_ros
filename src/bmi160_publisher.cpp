6fa07b73f2edaddb62b31c41066193071f202af6
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2024-05-24 14:01:46 +0000

    Update pkf

--- src/bmi160_publisher.cpp
+++ src/bmi160_publisher.cpp
@@ -13,6 +13,9 @@ public:
         nh.getParam("gyro_odr", gyro_odr_);
         nh.getParam("gyro_range", gyro_range_);
         nh.getParam("gyro_bw", gyro_bw_);
+        nh.getParam("imu_frame_id", imu_frame_id_);
+
+        printConfiguration();
 
         imu_pub_ = nh.advertise<sensor_msgs::Imu>("imu/data", 10);
 
@@ -35,10 +38,23 @@ private:
     ros::Publisher imu_pub_;
     double publish_rate_;
     int accel_odr_, accel_range_, accel_bw_, gyro_odr_, gyro_range_, gyro_bw_;
+    std::string imu_frame_id_;
     BMI160 imu_;
     bmi160_dev sensor_;
     uint32_t prev_sensor_time_;
     ros::Time last_ros_time_;
+
+    void printConfiguration() {
+        ROS_INFO("Sensor configuration:");
+        ROS_INFO("  Publish rate: %f Hz", publish_rate_);
+        ROS_INFO("  Accelerometer ODR: 0x%02X", accel_odr_);
+        ROS_INFO("  Accelerometer range: 0x%02X", accel_range_);
+        ROS_INFO("  Accelerometer bandwidth: 0x%02X", accel_bw_);
+        ROS_INFO("  Gyroscope ODR: 0x%02X", gyro_odr_);
+        ROS_INFO("  Gyroscope range: 0x%02X", gyro_range_);
+        ROS_INFO("  Gyroscope bandwidth: 0x%02X", gyro_bw_);
+        ROS_INFO("  IMU frame ID: %s", imu_frame_id_.c_str());
+    }
 
     bool initializeIMU() {
         int8_t rslt = BMI160_OK;
@@ -75,6 +91,7 @@ private:
             }
 
             imu_msg.header.stamp = last_ros_time_;
+            imu_msg.header.frame_id = imu_frame_id_;
             imu_msg.linear_acceleration.x = convertRawAccelToMs2(acc_data.x, accel_range_);
             imu_msg.linear_acceleration.y = convertRawAccelToMs2(acc_data.y, accel_range_);
             imu_msg.linear_acceleration.z = convertRawAccelToMs2(acc_data.z, accel_range_);
